4ef1bcdf435393f01f8ba4ca124bdfa7f8d503d4
ogranesson <shresthaanshu555@gmail.com>
2025-01-16 02:38:19 +0100

    Add coloring cells

--- game_of_life.py
+++ game_of_life.py
@@ -7,6 +7,8 @@ GRID_SIZE_X = 50
 GRID_SIZE_Y = 50
 CELL_SIZE = 20
 
+GRID = [[0 for _ in range(GRID_SIZE_Y)] for _ in range(GRID_SIZE_X)]
+
 board = pygame.display.set_mode([GRID_SIZE_X * CELL_SIZE, GRID_SIZE_Y * CELL_SIZE])
 pygame.display.set_caption("Conway's game of life")
 timer = pygame.time.Clock()
@@ -15,29 +17,34 @@ game_running = True
 
 def draw():
     board.fill((0, 0, 0))
-    for i in range(0, GRID_SIZE_Y * CELL_SIZE, CELL_SIZE):
-        for j in range(0, GRID_SIZE_X * CELL_SIZE, CELL_SIZE):
-            pygame.draw.rect(board, (255, 255, 255), (i + 1, j + 1, CELL_SIZE - 1, CELL_SIZE - 1))
+    for row in range(GRID_SIZE_X):
+        for col in range(GRID_SIZE_Y):
+            if GRID[row][col] == 0:
+                pygame.draw.rect(board, (255, 255, 255), (row * CELL_SIZE + 1, col * CELL_SIZE + 1, CELL_SIZE - 1, CELL_SIZE - 1))
+            else:
+                pygame.draw.rect(board, (255, 0, 0), (row * CELL_SIZE + 1, col * CELL_SIZE + 1, CELL_SIZE - 1, CELL_SIZE - 1))
 
-def move():
-    global X
-    global Y
+def update_cell(row, column):
+    if GRID[row][column] == 0:
+        GRID[row][column] = 1
+    else:
+        GRID[row][column] = 0
 
-    X += 10
-    Y += 10
-
-    board.fill((0, 0, 0))
-    pygame.draw.rect(board, (255, 0, 0), (X, Y, SQUARE, SQUARE))
+    pygame.display.flip()
 
 try:
     while game_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    move()
                     pygame.display.flip()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                row = mouse_x // CELL_SIZE
+                column = mouse_y // CELL_SIZE
+                update_cell(row, column)
 
         draw()
         pygame.display.flip()
